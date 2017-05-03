47ea92e8f525d9b793f91cbc467c7b3125db8d2d
Zackstrife7 <zackstrife34000@gmail.com>
2017-05-03 11:26:58 +0200

    form
    formulaire inscription timesaviours

---
+++ form.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>formulaire</title>
+	</head>
+	<body>
+	<legend>Connexion:</legend>
+	<fieldset>
+		<form>
+			<input type="text" name="login" placeholder="rentrez votre login...">
+			<input type="password" name="pass">
+			<input type="submit" name="login">
+		</form>
+	</fieldset>	
+	</body>
+</html>
