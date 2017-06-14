d15dec49612daa95dc2cf02db6bf04c8b9e90125
guss34 <guillaume.gatty@gmail.com>
2017-06-14 15:10:27 +0200

    images pour test

---
+++ classes/imageModel.php
@@ -0,0 +1,16 @@
+<?php
+require_once('../core/coreModel.php');
+
+class imageModel extends CoreModel
+{
+  /**************************************************************************
+    fonction qui recupere les infos images
+    @param : $id=> id de l'image
+  ***************************************************************************/
+  public function getImage($id){
+    $req = 'SELECT * FROM `image` WHERE i_id = :idImage';
+    $options = array('idImage'=> $id);
+    return parent::executeQuery($req,$options)
+
+  }
+}
